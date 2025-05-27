7dd6c06def1364799242649253c11e04169dd759
Mike3rd <mturko@outlook.com>
2025-05-26 20:35:14 -0400

    fix pw

--- login.php
+++ login.php
@@ -6,13 +6,13 @@ $valid_password = 'mypassword'; // CHANGE THIS
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['username'] === $valid_username && $_POST['password'] === $valid_password) {
         $_SESSION['logged_in'] = true;
-        header("Location: protected.php");
+        header("Location: index.html");
         exit();
     } else {
         echo "<p>Invalid credentials. <a href='index.php'>Try again</a>.</p>";
     }
 } else {
-    header("Location: index.html");
+    header("Location: index.php");
     exit();
 }
 ?>
